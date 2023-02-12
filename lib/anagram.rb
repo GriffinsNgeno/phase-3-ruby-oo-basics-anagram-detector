3fa52da52f2a9dd971fd0ab0b4f2dc03aa82954f
GriffinsNgeno <griffins.ngeno@student.moringaschool.com>
2023-02-12 22:03:34 +0300

    Completed Lab

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,11 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+    def initialize(word)
+        @word = word
+    end
+    def match list
+        list.filter {|myword| myword.chars.sort == @word.chars.sort}
+
+    end
+end
